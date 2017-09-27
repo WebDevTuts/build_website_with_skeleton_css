279e9ba5d3cd6a88cb895bc351bb2a2cc59c4a1e
Edwin Chen <edwinchen85@gmail.com>
2017-09-28 06:11:17 +0800

    Add css gulp task to concat, uncss and clean css files.

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,25 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
+var concatCss = require('gulp-concat-css');
+var cleanCss = require('gulp-clean-css');
+var uncss = require('gulp-uncss');
 
-gulp.task('serve', function() {
+gulp.task('serve', ['css'], function() {
 
   browserSync.init({
     server: "./"
   });
 
-  gulp.watch(["./*.html", "./css/*.css"]).on('change', browserSync.reload);
+  gulp.watch(["./*.html", "./css/*.css", ['css']]).on('change', browserSync.reload);
 
+});
+
+gulp.task('css', function() {
+  return gulp.src(['./css/normalize.css', './css/skeleton.css', './css/main.css'])
+          .pipe(concatCss('bundle.css'))
+          .pipe(uncss({html: ['index.html']}))
+          .pipe(cleanCss({compatibility: 'ie8'}))
+          .pipe(gulp.dest('css'));
 });
 
 gulp.task('default', ['serve']);
